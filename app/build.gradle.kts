5cdaa102f3ad45047ca528835f06451db538e409
onecoin <one_coin@naver.com>
2024-07-18 08:03:19 +0900

    add ui 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 //    implementation(libs.firebase.crashlytics)
+
 }
---
+++ app/src/main/java/com/example/project_sns/chat/ChatListFragment.kt
@@ -0,0 +1,36 @@
+package com.example.project_sns.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.project_sns.R
+import com.example.project_sns.databinding.FragmentChatListBinding
+
+
+class ChatListFragment : Fragment() {
+
+    private var _binding : FragmentChatListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatListBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
